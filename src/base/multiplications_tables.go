72e7c9e6cdb36d202267975913c63fea1e85c755
mosy <479979298@qq.com>
2019-03-19 14:23:52 +0800

    ADD

--- src/base/multiplications_tables.go
+++ src/base/multiplications_tables.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 )
 
-func main() {
+func test() {
 	var j, i int
 	for i = 1; i < 10; i++ {
 		for j = 1; j <= i; j++ {
 			//fmt.Printf("%d \n", i, "*", j, "=", i*j)
 			fmt.Printf("%d * %d = %d\t", j, i, i*j)
 		}
-		fmt.Println("")
+		fmt.Println()
 	}
+}
+func main() {
+	test()
 }
--- src/hello/hello_go.go
+++ src/hello/hello_go.go
@@ -4,6 +4,37 @@ import (
 	"fmt"
 )
 
+//const (
+//	a = iota
+//	b
+//	c
+//	d = "ha"
+//	e
+//	f = 100
+//	h
+//	i = iota
+//	g
+//)
+func swap(x int, y int) int {
+	return x + y
+}
 func main() {
-	fmt.Printf("Hello  GO !")
+	//var str string
+	//str := "asfafa"
+	//const b string = "sdfsf"
+	//fmt.Printf("Hello  GO !", b)
+	//fmt.Println(a, b, c, d, e, f, h, i, g)
+	o, p := 1, 2
+	if o == p {
+		fmt.Println("执行aaaa")
+	} else {
+		fmt.Println("执行BB")
+	}
+
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
+	}
+	x, y := 1, 5
+	fmt.Println(swap(x, y))
+
 }
